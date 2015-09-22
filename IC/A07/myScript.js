1c7887d3ea5c66fe4381486b51734100fc36db10
Sean2by4 <sean2by4@gmail.com>
2015-09-22 02:01:58 -1000

    aa

--- IC/A07/myScript.js
+++ IC/A07/myScript.js
@@ -1,16 +1,36 @@
-getClasses(elem)
+function getClasses(elem)
 {
     var classList = document.getElementById(elem).className.split(/\s+/);
 };
 
-addClass(elem, className){
+function addClass(elem, className){
     elem.classList.add(className);
 }
 
 
 
-validateForm()
+function validateForm()
 {
-
-    
+    var un=document.getElementById('username').value;
+    var em=document.getElementById('email').value;
+    var pa=document.getElementById('password').value;
+    var co=document.getElementById('confirm').value;
+    if(un == ""){
+        alert('Please Enter First Name');
+        document.getElementById('username').style.borderColor = "red";
+    }else{
+        document.getElementById('username').style.borderColor = "green";
+    }
+    if (/^[0-9]+$/.test(document.getElementById("username").value)) {
+        alert("First Name Contains Numbers!");
+        document.getElementById('username').style.borderColor = "red";
+    }else{
+        document.getElementById('username').style.borderColor = "green";
+    }
+    if(un.length <=2){
+        alert('Your Name is To Short');
+        document.getElementById('username').style.borderColor = "red";
+    }else{
+        document.getElementById('username').style.borderColor = "green";
+    }
 }
