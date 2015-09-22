36a29b90d0aacb8681f38c28a2da87d7b9ec8d84
Sean2by4 <sean2by4@gmail.com>
2015-09-22 01:28:28 -1000

    a07

---
+++ IC/A07/myScript.js
@@ -0,0 +1,16 @@
+getClasses(elem)
+{
+    var classList = document.getElementById(elem).className.split(/\s+/);
+};
+
+addClass(elem, className){
+    elem.classList.add(className);
+}
+
+
+
+validateForm()
+{
+
+    
+}
